b0cab0765efa3ff82c08b69e0eb4df525d4e6959
Nestor Mendoza <nestor.mendozam@gmail.com>
2025-11-17 17:01:40 -0600

    feat: Implement product management features including create, edit, delete, and search functionalities
    - Added new components for creating, editing, and deleting products.
    - Implemented product showcase and search functionalities.
    - Updated user service to handle admin authentication.
    - Refactored product service to interact with the products microservice.
    - Removed obsolete showcase component and related files.
    - Enhanced form validation for product management forms.
    - Updated environment configuration to include products microservice URL.

--- eCommerce/src/environment.ts
+++ eCommerce/src/environment.ts
@@ -1,6 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api/auth/', // API base URL for development
+    apiUrl: 'https://localhost:5001/api/auth/', // API base URL for development,
+    productsMicroserviceUrl: 'https://localhost:5002/api/products/'
   };
   
