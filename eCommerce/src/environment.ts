b2a673867e616e7f019ea386b6c961cb36f107d0
Nestor Mendoza <nestor.mendozam@gmail.com>
2025-11-20 10:51:39 -0600

    fix: Correct API URLs in environment configuration for development

--- eCommerce/src/environment.ts
+++ eCommerce/src/environment.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api/auth/', // API base URL for development,
-    productsMicroserviceUrl: 'https://localhost:5002/api/products/'
+    apiUrl: 'http://localhost:5001/api/auth/', // API base URL for development,
+    productsMicroserviceUrl: 'http://localhost:5002/api/products/'
   };
   
